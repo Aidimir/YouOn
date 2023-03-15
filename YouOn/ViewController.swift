e6c235ff1e958b92786f0c1d55c4fe7607e7c93b
Aidimir <aidimir1@mail.ru>
2023-03-15 13:15:30 +0500

    Initial Commit

---
+++ YouOn/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YouOn
+//
+//  Created by Айдимир Магомедов on 15.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
